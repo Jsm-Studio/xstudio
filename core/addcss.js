27c453cedb9ba8a19477624ece32e139cb2ec5a8
NoNameTitan <50516342+x-titan@users.noreply.github.com>
2021-06-12 16:46:42 +0500

    css

--- core/addcss.js
+++ core/addcss.js
@@ -8,7 +8,6 @@ d.head.appendChild(d.createElement("style")).innerHTML = `@import url('https://f
 ::-webkit-scrollbar-thumb:hover{background:var(--gray-400)}body{overflow:hidden;height:100vh;width:100vw;font-family:'Roboto',sans-serif;font-size:16px}
 p,a,h1,h2,h3,h4,h5,h6,i,span,div,b,strong,abbr,code,texе,font,ya-tr-span{color:inherit;font-family:inherit;font-size:inherit;font-weight:inherit}
 h1,h2,h3,h4,h5,h6{font-weight:500;display:inline-block}h1{font-size:var(--h1)}h2{font-size:var(--h2)}h3{font-size:var(--h3)}h4{font-size:var(--h4)}h5{font-size:var(--h5)}
-h6{font-size:var(--h6)}strong{font-weight:700}abbr{text-decoration:underline var(--accent)2px;}mark{background-color:var(--accent);color:var(--light)}.Montserrat{font-family:'Montserrat',sans-serif}
-.Open_sans{font-family:'Open Sans',sans-serif}.Roboto{font-family:'Roboto',sans-serif}.Barlow{font-family:'Barlow',sans-serif;}.Comfortaa{font-family:'Comfortaa',sans-serif}
+h6{font-size:var(--h6)}strong{font-weight:700}abbr{text-decoration:underline var(--accent)2px;}mark{background-color:var(--accent);color:var(--light)}
 #app,#xbody,[xbuild]{width:var(--fill);height:var(--fill);position:relative}#app{overflow:hidden}#xbody,[xbody]{overflow:auto}[xsection]{overflow:hidden}
 .wrapper[xbuild]{display:flex;align-items:center;justify-content:center}[xtext]{display:inline-block;width:fit-content;height:auto}[xlist],[xlist="column"]{display:flex;flex-direction:column}[xlist="row"]{flex-direction:row}`
