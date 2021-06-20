8eeeb09098a87bce59d895a8ae2b59ecad59bb0f
NoNameTitan <86165315+Jsm-Studio@users.noreply.github.com>
2021-06-20 16:22:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import XStudio from "./core/xstudio.js"
-import { XBlank, XContent, XSection, XHero, XList, XFooter, XText } from "./core/xbuild.js"
+import { XBlank, XContent, XSection, XHero, XList, XFooter, XText, XImage } from "./core/xbuild.js"
 import * as xtools from "./core/xtool.js"
 import "./core/addcss.js"
 
-export { XStudio, XBlank, XContent, XSection, XHero, XList, XFooter, XText, xtools }
+export { XStudio, XBlank, XContent, XSection, XHero, XList, XFooter, XText, XImage, xtools }
